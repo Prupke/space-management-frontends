cace67f0fc85a0b95ff866fbc75d940fc4177c72
Britt Prop <britt.prop@ziggo.nl>
2022-05-17 19:02:39 +0200

    initial version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Menu from './components/Menu.js';
+import Header from './components/Header.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Menu />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Header = () => {
+    return (
+        <div class="header">
+            <h3>Ka huis</h3>
+        </div>
+    )
+}
+export default Header
